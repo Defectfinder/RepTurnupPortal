138ee23794bbbcb3ed34d94735fb5e39e2b28327
Defectfinder <mkalpana705@gmail.com>
2024-08-15 22:26:35 +1200

    inital code commit for turnup

---
+++ RepTurnupProtal/RepTurnupProtal/Program.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        //open chrome brower
+        IWebDriver driver = new ChromeDriver();
+        //Launch turnup portal
+        driver.Navigate().GoToUrl("http://horse.industryconnect.io/");
+        driver.Manage().Window.Maximize();
+
+        //Identify Username textbox
+        IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+        usernameTextbox.SendKeys("hari");
+        //identify password
+        IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+        passwordTextbox.SendKeys("123123");
+
+        // Identify login button and click it
+        IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+        loginButton.Click();
+
+        // check the login button
+        IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+        if (helloHari.Text == "Hello hari!")
+        {
+            Console.WriteLine("User login successfully. Test Passed");
+        }
+        else
+        {
+            Console.WriteLine("user has not logged in. Test failed");
+        }
+    }
+}
